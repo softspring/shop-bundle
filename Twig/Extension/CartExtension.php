c65193c708100885fb6df4f524a6e070f5bb796a
Javi H. Gil <javihgil@gmail.com>
2019-10-18 13:41:06 +0200

    Cart twig extension

---
+++ Twig/Extension/CartExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Softspring\ShopBundle\Twig\Extension;
+
+use Softspring\ShopBundle\Manager\CartManagerInterface;
+use Softspring\ShopBundle\Model\OrderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class CartExtension extends AbstractExtension
+{
+    /**
+     * @var CartManagerInterface
+     */
+    protected $cartManager;
+
+    /**
+     * CartExtension constructor.
+     *
+     * @param CartManagerInterface $cartManager
+     */
+    public function __construct(CartManagerInterface $cartManager)
+    {
+        $this->cartManager = $cartManager;
+    }
+
+    /**
+     * @return array|TwigFunction[]
+     */
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('cart', [$this, 'getCart']),
+        ];
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return OrderInterface|null
+     */
+    public function getCart(Request $request): ?OrderInterface
+    {
+        return $this->cartManager->getCart($request);
+    }
+}
