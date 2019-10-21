00865f7b3a54340e0d8d937c491b816eba4ff70b
Javi H. Gil <javihgil@gmail.com>
2019-10-21 12:41:06 +0200

    Use SfsCoreBundle doctrine target entity resolver abstract compiler pass

--- DependencyInjection/Compiler/ResolveDoctrineTargetEntityPass.php
+++ DependencyInjection/Compiler/ResolveDoctrineTargetEntityPass.php
@@ -2,67 +2,33 @@
 
 namespace Softspring\ShopBundle\DependencyInjection\Compiler;
 
+use Softspring\CoreBundle\DependencyInjection\Compiler\AbstractResolveDoctrineTargetEntityPass;
 use Softspring\ShopBundle\Model\CustomerInterface;
 use Softspring\ShopBundle\Model\OrderInterface;
 use Softspring\ShopBundle\Model\OrderEntryInterface;
 use Softspring\ShopBundle\Model\SalableItemInterface;
 use Softspring\ShopBundle\Model\StoreInterface;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Exception\LogicException;
 
-class ResolveDoctrineTargetEntityPass implements CompilerPassInterface
+class ResolveDoctrineTargetEntityPass extends AbstractResolveDoctrineTargetEntityPass
 {
+    /**
+     * @inheritDoc
+     */
+    protected function getEntityManagerName(ContainerBuilder $container): string
+    {
+        return $container->getParameter('sfs_shop.entity_manager_name');
+    }
+
     /**
      * @param ContainerBuilder $container
      */
     public function process(ContainerBuilder $container)
     {
-        // configure customer
-        $customerClass = $container->getParameter('sfs_shop.customer.class');
-        if (!class_implements($customerClass, CustomerInterface::class)) {
-            throw new LogicException(sprintf('%s class must implements %s interface', $customerClass, CustomerInterface::class));
-        }
-        $this->setTargetEntity($container, CustomerInterface::class, $customerClass);
-
-        // configure salable
-        $salableItemClass = $container->getParameter('sfs_shop.salable_item.class');
-        if (!class_implements($salableItemClass, SalableItemInterface::class)) {
-            throw new LogicException(sprintf('%s class must implements %s interface', $salableItemClass, SalableItemInterface::class));
-        }
-        $this->setTargetEntity($container, SalableItemInterface::class, $salableItemClass);
-
-        // configure order
-        $orderClass = $container->getParameter('sfs_shop.order.class');
-        if (!class_implements($orderClass, OrderInterface::class)) {
-            throw new LogicException(sprintf('%s class must implements %s interface', $orderClass, OrderInterface::class));
-        }
-        $this->setTargetEntity($container, OrderInterface::class, $orderClass);
-
-        // configure order item
-        $orderEntryClass = $container->getParameter('sfs_shop.order.entry.class');
-        if (!class_implements($orderEntryClass, OrderEntryInterface::class)) {
-            throw new LogicException(sprintf('%s class must implements %s interface', $orderEntryClass, OrderEntryInterface::class));
-        }
-        $this->setTargetEntity($container, OrderEntryInterface::class, $orderEntryClass);
-
-        // configure store
-        if ($storeClass = $container->getParameter('sfs_shop.store.class')) {
-            if (!class_implements($storeClass, StoreInterface::class)) {
-                throw new LogicException(sprintf('%s class must implements %s interface', $storeClass, StoreInterface::class));
-            }
-            $this->setTargetEntity($container, StoreInterface::class, $storeClass);
-        }
-    }
-
-    private function setTargetEntity(ContainerBuilder $container, string $interface, string $class)
-    {
-        $resolveTargetEntityListener = $container->findDefinition('doctrine.orm.listeners.resolve_target_entity');
-
-        if (!$resolveTargetEntityListener->hasTag('doctrine.event_subscriber')) {
-            $resolveTargetEntityListener->addTag('doctrine.event_subscriber');
-        }
-
-        $resolveTargetEntityListener->addMethodCall('addResolveTargetEntity', [$interface, $class, [$container->getParameter('sfs_shop.entity_manager_name')]]);
+        $this->setTargetEntityFromParameter('sfs_shop.customer.class', CustomerInterface::class, $container, true);
+        $this->setTargetEntityFromParameter('sfs_shop.salable_item.class', SalableItemInterface::class, $container, true);
+        $this->setTargetEntityFromParameter('sfs_shop.order.class', OrderInterface::class, $container, true);
+        $this->setTargetEntityFromParameter('sfs_shop.order.entry.class', OrderEntryInterface::class, $container, true);
+        $this->setTargetEntityFromParameter('sfs_shop.store.class', StoreInterface::class, $container, false);
     }
 }
